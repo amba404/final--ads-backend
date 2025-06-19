c77495860fd3bf845a343b39f45d497ceab5ba5a
amba404 <amba.avg@gmail.com>
2025-06-19 17:35:26 +0900

    Добавлены ограничения dto.Login

--- src/main/java/ru/skypro/homework/dto/Login.java
+++ src/main/java/ru/skypro/homework/dto/Login.java
@@ -1,10 +1,17 @@
 package ru.skypro.homework.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class Login {
 
+    @NotEmpty
+    @Size(min = 4, max = 32)
     private String username;
+
+    @NotEmpty
+    @Size(min = 8, max = 16)
     private String password;
 }
